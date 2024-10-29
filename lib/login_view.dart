30633dec3155e6f3058d57d38403532bc5f64aa9
le-vitalique <site.lab7@gmail.com>
2024-10-29 11:04:34 +0400

    Initial commit

---
+++ lib/login_view.dart
@@ -0,0 +1,90 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  LoginView({Key? key}) : super(key: key);
+
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _fieldNameCtr = TextEditingController();
+  final TextEditingController _fieldEmailCtr = TextEditingController();
+  final TextEditingController _fieldPhoneCtr = TextEditingController();
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.reset();
+      print(_fieldNameCtr.text);
+      print(_fieldEmailCtr.text);
+      print(_fieldPhoneCtr.text);
+
+      //print('Submit');
+    }
+  }
+
+  bool _isNumeric(String s) {
+    if (s == null) {
+      return false;
+    }
+    return double.tryParse(s) != null;
+  }
+
+  String? _validatePhone(value) {
+    if (_isNumeric(value) == false)
+      return 'Заполните поле Phone';
+    else
+      return null;
+  }
+
+  String? _validateEmail(value) {
+    if (EmailValidator.validate(value) == false)
+      return 'Заполните поле Email';
+    else
+      return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                controller: _fieldNameCtr,
+                decoration: const InputDecoration(labelText: 'Name'),
+                validator: (value) {
+                  if (value == '')
+                    return 'Заполните поле Name';
+                  else
+                    return null;
+                },
+              ),
+              TextFormField(
+                controller: _fieldEmailCtr,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: _validateEmail,
+              ),
+              TextFormField(
+                controller: _fieldPhoneCtr,
+                decoration: const InputDecoration(labelText: 'Phone'),
+                validator: _validatePhone,
+              ),
+              TextButton(onPressed: _submitForm, child: const Text('Отправить')),
+
+              TextButton(
+                child: const Text('Назад'),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/home');
+                },
+              ),
+
+            ],
+          ),
+        ));
+  }
+}
